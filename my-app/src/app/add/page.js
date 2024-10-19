91bc82efe0dd272f8d03bc67adf6d5fb9534c5b9
Jonathan Lam <jonhaolam@gmail.com>
2024-10-19 00:23:36 -0400

    Adjusted css file path

--- my-app/src/app/add/page.js
+++ my-app/src/app/add/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import 'AddMovie.css';
+import './AddMovie.css';
 
 // src/app/add/AddMovie.js
 const AddMovie = () => {
